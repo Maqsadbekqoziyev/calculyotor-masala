b4a15fdc87888151c76151b9036d2deab1bf9378
Navrozbek01 <maqsadbekqoziyev245@gmail.com>
2025-12-05 17:59:14 +0500

    calculyator
    bu yerda qoshimch bor!

---
+++ calculyotor masala.py
@@ -0,0 +1,42 @@
+# def calculator(x, y, belgi):
+#     if belgi == "+":
+#         return f"Yigindi: {x + y}"
+#     elif belgi == "-":
+#         return f"ayirma: {x - y}"
+#     elif belgi == "*":
+#         return f"Ko'paytma: {x * y}"
+#     elif belgi == "/":
+#         return f"Bo'linma: {x / y}"
+#     elif belgi == "**":
+#         return f"Daraja :  {x ** y}"
+#     elif belgi == "%":
+#         return f"qoldiqli bo'lish : {x % y}"
+#     elif belgi == "//":
+#         return f"butun bolish : {x // y}"
+#     else:
+#          return f"qiymat xato kiritildi!\nIltimos qaytadan urinib ko'ring!"
+#
+#
+#
+# x = int(input("X: "))
+# y = int(input("Y: "))
+# belgi = input("Belgi(+, -, *, /, **, %, //):")
+# print(calculator(x,y,belgi))
+
+
+
+##2-masala
+# def teskari(soz):
+#     x = soz[::-1]
+#     return F"{soz} ni teskari: {x}"
+#
+# x = input("So'zni kiriting: ")
+#
+# print(teskari(x))
+
+##3-masala
+# def katta(matn):
+#     x = matn.title()
+#     return x
+# soz = input("Matnni kiriting: ")
+# print(katta(soz))
